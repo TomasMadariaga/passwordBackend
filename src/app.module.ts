eafb767a7c02a9881ea379ecf71133013f655da9
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-06-28 15:31:49 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PasswordModule } from './password/password.module';
+import { PasswordService } from './password/password.service';
+import { PasswordController } from './password/password.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    PasswordModule,
+  ],
+  controllers: [AppController, AuthController, PasswordController],
+  providers: [AppService, AuthService, PasswordService],
+})
+export class AppModule {}
